2d40bac6248e5b60318f2d2a0f3e317403fe28f3
Torsten Kohn <tkohn@users.noreply.github.com>
2021-02-01 17:20:13 +0100

    Update App with the different calls for the exercises

--- src/main/java/dev/kohn/playground/App.java
+++ src/main/java/dev/kohn/playground/App.java
@@ -1,7 +1,23 @@
 package dev.kohn.playground;
 
+import dev.kohn.playground.parse.GraphGenerator;
+import dev.kohn.playground.tracing.DistributedTracingGraph;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello App!");
+
+        final DistributedTracingGraph distributedTracingGraph = new DistributedTracingGraph(
+                GraphGenerator.createDirectedWeightedGraphFromFile("/home/torsten/development/test.txt"));
+
+        System.out.println(" 1. " + distributedTracingGraph.averageLatencyFor("A", "B", "C"));
+        System.out.println(" 2. " + distributedTracingGraph.averageLatencyFor("A", "D"));
+        System.out.println(" 3. " + distributedTracingGraph.averageLatencyFor("A", "D", "C"));
+        System.out.println(" 4. " + distributedTracingGraph.averageLatencyFor("A", "E", "B", "C", "D"));
+        System.out.println(" 5. " + distributedTracingGraph.averageLatencyFor("A", "E", "D"));
+        System.out.println(" 6. " + distributedTracingGraph.numberOfTracesWithConfiguredHops("C", "C", 1, 3));
+        System.out.println(" 7. " + distributedTracingGraph.numberOfTracesWithConfiguredHops("A", "C", 4, 4));
+        System.out.println(" 8. " + distributedTracingGraph.latencyForShortestTraceBetween("A", "C"));
+        System.out.println(" 9. " + distributedTracingGraph.latencyForShortestTraceBetween("B", "B"));
+        System.out.println("10. skipped");
     }
 }
